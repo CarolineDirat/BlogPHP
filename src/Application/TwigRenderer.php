21b17fc956cf70357222b020b867c87696295d1d
CaroJo1986 <caroline.dirat@gmail.com>
2020-04-29 09:08:05 +0200

    add TwigRenderer class

---
+++ src/Application/TwigRenderer.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Application;
+
+class TwigRenderer 
+{
+
+    private $twig;
+
+    private $loader;
+
+    public function __construct(string $path)
+    {
+        $this->loader = new \Twig\Loader\FilesystemLoader($path);
+        $this->twig = new \Twig\Environment($this->loader, []);
+    }
+
+    /**
+     * Permet de rajouter un chemin pour charger les vues
+     * @param string $namespace
+     * @param null|string $path
+     */
+    public function addPath(string $namespace, ?string $path = null): void
+    {
+        $this->loader->addPath($path, $namespace);
+    }
+
+    /**
+     * Permet de rendre une vue
+     * Le chemin peut être précisé avec des namespace rajoutés via addPath()
+     * @param string $view
+     * @param array $params
+     * @return string
+     */
+    public function render(string $view, array $params = []): string
+    {
+        return $this->twig->render($view . '.twig', $params);
+    }
+
+    /**
+     * Permet de rajouter des variables globales à toutes les vues
+     *
+     * @param string $key
+     * @param mixed $value
+     */
+    public function addGlobal(string $key, $value): void
+    {
+        $this->twig->addGlobal($key, $value);
+    }
+}
