3fc80d7865eb8d24ede130b8e7ae859b1d63b750
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-06 13:39:45 +0200

    sanitize superglobal variables in HTTPRequest

--- src/Application/HTTPRequest.php
+++ src/Application/HTTPRequest.php
@@ -10,11 +10,11 @@ final class HTTPRequest // extends ApplicationComponent
      * get a cookie variable if it exists
      *
      * @param  string $key
-     * @return mixed
+     * @return string
      */
     public function cookieData($key)
     {
-        return isset($_COOKIE[$key]) ? $_COOKIE[$key] : null;
+        return isset($_COOKIE[$key]) ?? filter_var($_COOKIE[$key], FILTER_SANITIZE_STRING);
     }
    
     /**
@@ -32,11 +32,11 @@ final class HTTPRequest // extends ApplicationComponent
      * get a session variable if it exists
      *
      * @param  string $key
-     * @return mixed
+     * @return string
      */
     public function sessionData($key)
     {
-        return isset($_SESSION[$key]) ? $_SESSION[$key] : null;
+        return isset($_SESSION[$key]) ?? filter_var($_SESSION[$key], FILTER_SANITIZE_STRING);
     }
    
     /**
@@ -54,11 +54,11 @@ final class HTTPRequest // extends ApplicationComponent
      * get a GET variable if it exists
      *
      * @param  string $key
-     * @return mixed
+     * @return string
      */
     public function getData($key)
     {
-        return isset($_GET[$key]) ? htmlspecialchars($_GET[$key]) : null;
+        return isset($_GET[$key]) ?? filter_var($_GET[$key], FILTER_SANITIZE_STRING);
     }
    
     /**
@@ -76,11 +76,11 @@ final class HTTPRequest // extends ApplicationComponent
      * get a POST variable if it exists
      *
      * @param  string $key
-     * @return mixed
+     * @return string
      */
     public function postData($key)
     {
-        return isset($_POST[$key]) ? htmlspecialchars($_POST[$key]) : null;
+        return isset($_POST[$key]) ?? filter_var($_POST[$key], FILTER_SANITIZE_STRING);
     }
    
     /**
@@ -101,7 +101,7 @@ final class HTTPRequest // extends ApplicationComponent
      */
     public function requestURI()
     {
-        return $_SERVER['REQUEST_URI'];
+        return filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_STRING);
     }
 
     /**
@@ -111,6 +111,6 @@ final class HTTPRequest // extends ApplicationComponent
      */
     public function method()
     {
-        return $_SERVER['REQUEST_METHOD'];
+        return filter_var($_SERVER['REQUEST_METHOD'], FILTER_SANITIZE_STRING);
     }
 }
