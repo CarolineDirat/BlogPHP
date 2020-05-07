a79dc9b04b6bab3f685af227c6d847efbdaec122
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-07 14:34:41 +0200

    fix src/Model/PostManagerPDO.php - query() instead of prepare() !!

--- src/Model/PostManagerPDO.php
+++ src/Model/PostManagerPDO.php
@@ -38,24 +38,26 @@ final class PostManagerPDO extends PostManager
             throw new \Exception('PostManangerPDO must use an instance of PDO to connect to a MySQL database');
         }
         
-        $sql = 'SELECT  id, title, slug, content, abstract, date_creation as dateCreation, date_update as dateUpdate, id_user as idUser FROM post ORDER BY dateCreation';
+        $sql = 'SELECT  id, title, slug, content, abstract, date_creation as dateCreation, date_update as dateUpdate, id_user as idUser FROM post ORDER BY dateUpdate';
         
         if($offset != -1 || $limit != -1){
             $sql .= ' LIMIT '.(int)$limit.' OFFSET '.(int)$offset;
         }
-        $req = $this->dao->prepare($sql);
+        $req = $this->dao->query($sql);
 
-        $req->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, 'Post', []);
-    
+        $req->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\App\Entity\Post', []);
+        
         $listPosts = $req->fetchAll();
         
         foreach ($listPosts as $post)
         {
-            $post->setDateCreation(new \DateTime($post->dateCreation()));
-            $post->setDateUpdate(new \DateTime($post->dateUpdate()));
+            $post->setDateCreation(new \DateTime($post->getDateCreation()));
+            $post->setDateUpdate(new \DateTime($post->getDateUpdate()));
         }
         
         $req->closeCursor();
+
+        
         
         return $listPosts;
     }
