b9c1f6d6a37a24ca13902b56259538f928130a68
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-11 16:03:25 +0200

    fix a problem of exception message after the display page (again...)

--- public/index.php
+++ public/index.php
@@ -9,32 +9,37 @@ use App\Application\HTTPResponse;
 use App\Application\TwigRenderer;
 
 try {
+    $match = false; // will be true if the rooter receive a route corresponding to a controller
     $action = "show";
     $page = "home";
     $httpRequest = new HTTPRequest();
     $twigRenderer = new TwigRenderer('../templates');
     
     if ($httpRequest->requestURI() === "/") {
+        $match = true;
         $controller = new HomeController($action, $page, $httpRequest);
         $httpResponse = $controller->execute();
         echo $twigRenderer->render($httpResponse->getPage(),$httpResponse->getParams());
-    }
+    } 
 
     if ($httpRequest->hasGET('page')) {
         $page =$httpRequest->getData('page');
         switch ($page) {
             case 'post':
+                $match = true;
                 $controller = new PostController($action, $page, $httpRequest);
                 $httpResponse = $controller->execute();
                 echo $twigRenderer->render($httpResponse->getPage(),$httpResponse->getParams());
             break;
             case 'blog':
+                $match = true;
                 $controller = new PostController($action, $page, $httpRequest);
                 $httpResponse = $controller->execute();
                 echo $twigRenderer->render($httpResponse->getPage(),$httpResponse->getParams());
             break;
             case 'contact':
                 if(isset($_POST)){
+                    $match = true;
                     $action = 'process';
                     $controller = new HomeController($action, $page, $httpRequest);
                     $httpResponse = $controller->execute();
@@ -43,7 +48,14 @@ try {
             break;
         }
     }
-    throw new \Exception('No page corresponds to that requested');
+
+    
+    
+    if(!$match) {
+        throw new \Exception('No page corresponds to that requested');
+    }
+    
+    
 } catch (Exception $e) {
     $twigRenderer = new TwigRenderer('../templates');
     echo $twigRenderer->render('error', ['error' => $e->getMessage()]);
