d5e060486f9b9fa8f7562a8811de5788ee4a8449
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-12 14:31:24 +0200

    creation of PHPMailerApp (extends PHPMailer) - lightens the HomeController code, and can define other functions for sending mail

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,10 +4,7 @@ namespace App\Controller;
 
 use App\Application\AbstractController;
 use App\Application\HTTPResponse;
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
+use App\Application\PHPMailerApp;
 
 //use App\Application\TwigRenderer;
 
@@ -48,36 +45,13 @@ final class HomeController extends AbstractController
         $lastName = $this->httpRequest->postData('lastName');
         $email = $this->httpRequest->postData('email1');
         $messageContact = $this->httpRequest->postData('messageContact');
+        $recipient = EMAIL_CONTACT; // @phpstan-ignore-line
 
         ////////// Create the email and send the message //////////////////
-        // Instantiation of PHPMailer and passing `true` enables exceptions
-        $mail = new PHPMailer(true);
-        $mail->set('CharSet', PHPMailer::CHARSET_UTF8);  // define the character set of the message at utf-8.
+        $mail = new PHPMailerApp(true);    // Instantiation of PHPMailer and passing `true` enables exceptions
+        $mail->settings();              // configurations of settings
         
-        //Server settings
-        $mail->SMTPDebug = 0;                      // Enable verbose debug output with SMTP::DEBUG_SERVER
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = SMTP_SERVER;  // @phpstan-ignore-line   // Set the SMTP server to send through, defined in config.php
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = SMTP_USER;    // @phpstan-ignore-line   // SMTP username, defined in config.php
-        $mail->Password   = SMTP_PASS;    // @phpstan-ignore-line   // SMTP password, defined in config.php
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-        //Recipients
-        $mail->setFrom('ne-pas-repondre@carocode.com', 'Contact Form');
-        $mail->addAddress(EMAIL_CONTACT); // @phpstan-ignore-line   // Add a recipient (Name is optional)
-        $mail->addReplyTo($email, $firstName.' '.$lastName);
-
-        // Content
-        $mail->isHTML(true);                                                                // Set email format to HTML
-        $mail->Subject = "Formulaire de contact à CaroCode : ".$firstName." ".$lastName;    // Here is the subject
-        // This is the HTML message body <b>in bold!</b>:
-        $mail->Body    = "Vous avez reçu un message depuis le formulaire de contact de CaroCode. Voici les details :<br/><br/><b>Prénom : </b>".$firstName."<br/><br/><b>Nom :</b> ".$lastName."<br/><br/><b>Email :</b> ".$email."<br/><br/><b>Message :</b> ".$messageContact;
-        // This is the body in plain text for non-HTML mail clients:
-        $mail->AltBody = $messageContact ;
-        
-        if ($mail->send()) {
+        if ($mail->sendContact($recipient, $firstName, $lastName, $email, $messageContact)) {
             return new HTTPResponse('home', ['messageInfo' => "Votre message a bien été envoyé."]);
         } 
         throw new \Exception("L'envoie du mail a échoué :".$mail->ErrorInfo);
