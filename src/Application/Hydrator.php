65f2d5b6875a5967f88d0c6b6466c4a9005b16a9
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-15 16:44:01 +0200

    create an Hydrator trait for hydrate() method used in Entity and Field classes

---
+++ src/Application/Hydrator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Application;
+
+trait Hydrator
+{
+    /**
+     * hydrate.
+     *
+     * hydrate class's properties with array data
+     */
+    public function hydrate(array $data): void
+    {
+        foreach ($data as $key => $value) {
+            $method = 'set'.ucfirst($key);
+            if (is_callable([$this, $method])) {
+                $this->$method($value);
+            }
+        }
+    }
+}
