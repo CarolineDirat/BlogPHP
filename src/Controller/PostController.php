baa2d4494815e8ac103499114a055b46ad326774
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-21 15:40:17 +0200

    now, PostController's executeShowPost() get list of comments corresponding to the post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -7,6 +7,7 @@ use App\Application\HTTPResponse;
 use App\Application\PDOSingleton;
 use App\Model\PostManagerPDO;
 use App\Model\UserManagerPDO;
+use App\Model\CommentManagerPDO;
 use Âpp\Entity\Post;
 
 final class PostController extends AbstractController
@@ -19,15 +20,20 @@ final class PostController extends AbstractController
         $httpRequest = $this->getHTTPRequest();
 
         if ($httpRequest->hasGet('id') && $httpRequest->hasGet('slug')) {
+            $dao = PDOSingleton::getInstance()->getConnexion();
             // get the post from the id
-            $postManager = new PostManagerPDO(PDOSingleton::getInstance()->getConnexion());
+            $postManager = new PostManagerPDO($dao);
             $post = $postManager->getPost((int) $httpRequest->getData('id'));
 
             // get the author from idUser of $post
-            $userManager = new UserManagerPDO(PDOSingleton::getInstance()->getConnexion());
+            $userManager = new UserManagerPDO($dao);
             $pseudo = $userManager->getPseudo((int) $post->getIdUser());
 
-            return new HTTPResponse($this->getPage(), ['post' => $post, 'pseudo' => $pseudo]);
+            // get list of post's comments
+            $commentManager = new CommentManagerPDO($dao);
+            $listComments = $commentManager->getListComments($post->getId());
+
+            return new HTTPResponse($this->getPage(), ['post' => $post, 'pseudo' => $pseudo, 'comments' => $listComments]);
         }
 
         throw new \Exception('id or slug is missing in the request');
