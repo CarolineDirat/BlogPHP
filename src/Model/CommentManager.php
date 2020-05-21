6bd481ce2003d3ee86bcad3fdf6b2dd1840dc804
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-21 14:01:00 +0200

    create new class : src/Model/CommentManager.php

---
+++ src/Model/CommentManager.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model;
+
+use App\Application\Manager;
+use App\Entity\Comment;
+
+/**
+ * CommentManager.
+ *
+ * Contains the methods which concern the management of Comments
+ */
+abstract class CommentManager extends Manager
+{
+    /**
+     * getListComments.
+     *
+     * Method which returns a list of comments from most recent to oldest. 
+     * 
+     * @return array[Comment]
+     */
+    abstract public function getListComments(): array;
+}
