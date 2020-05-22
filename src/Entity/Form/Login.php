89d9aed141564a78844c35ad7bb752be9987562c
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-22 17:16:26 +0200

    create  src/Entity/Form/Login.php : entity for the login form

---
+++ src/Entity/Form/Login.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity\Form;
+
+use App\Application\Entity;
+use DateTime;
+
+/**
+ * User.
+ *
+ * Entity class that represents a User
+ */
+final class Login extends Entity
+{
+   /**
+     * username
+     * 
+     * property to manage honeypot field
+     *
+     * @var ?string
+     */
+    private ?string $username = null;
+    
+    /**
+     * pseudo
+     *
+     * @var ?string
+     */
+    private ?string $pseudo = null;
+    
+    /**
+     * password
+     *
+     * @var ?string
+     */
+    private ?string $password = null;
+
+    /**
+     * Get property to manage honeypot field
+     *
+     * @return  ?string
+     */ 
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set property to manage honeypot field
+     *
+     * @param  string  $username  property to manage honeypot field
+     *
+     * @return  self
+     */ 
+    public function setUsername(?string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get pseudo
+     *
+     * @return  ?string
+     */ 
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * Set pseudo
+     *
+     * @param  ?string  $pseudo  pseudo
+     *
+     * @return  self
+     */ 
+    public function setPseudo(?string $pseudo): self
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return  ?string
+     */ 
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set password
+     *
+     * @param  ?string  $password  password
+     *
+     * @return  self
+     */ 
+    public function setPassword(?string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
