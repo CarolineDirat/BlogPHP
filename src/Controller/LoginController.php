35a0d1157850fe790289ab7ee7d423860b597867
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-22 16:17:03 +0200

    complete executSowLogin() method of src/Controller/LoginController.php

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,6 +6,8 @@ use Exception;
 use App\Application\AbstractController;
 use App\Application\Form\Form;
 use App\Application\HTTPResponse;
+use App\Entity\User;
+use App\FormBuilder\LoginFormBuilder;
 
 final class LoginController extends AbstractController
 {
@@ -15,7 +17,10 @@ final class LoginController extends AbstractController
     public function executeShowLogin(): HTTPResponse
     {
         // Build login form
+        $user = new User();
+        $loginForm = new LoginFormBuilder($user);
 
         // return response
+        return new HTTPResponse($this->getPage(), ['loginForm' => $loginForm]);
     }
 }
