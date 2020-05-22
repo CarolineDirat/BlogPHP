d359bf2c0ff9bb9bf0e2b8e197a9082edf577d50
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-22 10:33:55 +0200

    add new route for login page : in  public/.htaccess and public/index.php

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use App\Application\AbstractController;
+use App\Application\Form\Form;
+use App\Application\HTTPResponse;
+
+final class LoginController extends AbstractController
+{
+    /**
+     * Controller to show the Login Page.
+     */
+    public function executeShowLogin(): HTTPResponse
+    {
+        // Build login form
+
+        // return response
+    }
+}
