a84968599564bb9973149552ff60df4be956b796
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-22 09:34:35 +0200

    improves src/Model/UserManagerPDO.php

--- src/Model/UserManagerPDO.php
+++ src/Model/UserManagerPDO.php
@@ -4,6 +4,7 @@ namespace App\Model;
 
 use Exception;
 use PDO;
+use PDOStatement;
 
 /**
  * UserManagerPDO.
@@ -14,12 +15,19 @@ final class UserManagerPDO extends UserManager
 {
     public function getPseudo(int $id): string
     {
+        if (!$this->dao instanceof PDO) {
+            throw new Exception('PostManangerPDO must use an instance of PDO to connect to a MySQL database');
+        }
         $req = $this->dao->prepare('SELECT pseudo FROM user WHERE id = :id');
+        if (!$req instanceof PDOStatement) {
+            throw new Exception('The SQL request failed');
+        }
         $req->bindValue(':id', (int) $id, PDO::PARAM_INT);
         $req->execute();
-        if ($pseudo = $req->fetch()) {
+        $pseudo = $req->fetch();
+        if ($pseudo) {
             return $pseudo['pseudo'];
-        }
+        } 
 
         throw new Exception('The nickname of the author of the article could not be recovered');
     }
