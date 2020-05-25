e747c34cb8fc0bf1965173b82a5405937e753eaf
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-25 16:19:39 +0200

    create src/FormHandler/LoginFormHandler.php to process login form and edit src/Controller/LoginController.php consequently

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,8 +5,12 @@ namespace App\Controller;
 use App\Application\AbstractController;
 //use App\Application\Form\Form;
 use App\Application\HTTPResponse;
+use App\Application\PDOSingleton;
 use App\Entity\Form\Login;
 use App\FormBuilder\LoginFormBuilder;
+use App\FormHandler\LoginFormHandler;
+use App\Model\UserManagerPDO;
+
 
 final class LoginController extends AbstractController
 {
@@ -15,7 +19,38 @@ final class LoginController extends AbstractController
      */
     public function executeShowLogin(): HTTPResponse
     {
-        // Build login form
+        if ('POST' === $this->httpRequest->method()) {
+            $login = new Login([
+                'username' => $this->httpRequest->postData('username'),
+                'pseudo' => $this->httpRequest->postData('pseudo'),
+                'password' => $this->httpRequest->postData('password')
+            ]);
+            $formBuilder = new LoginFormBuilder($login);
+            $formBuilder->build();
+            $loginForm = $formBuilder->getForm();
+            // check honeypot
+            if(!empty($login->getUsername()))
+            {
+                return new HTTPResponse($this->getPage(), ['loginForm' => $loginForm]);
+            }
+            $manager = new UserManagerPDO(PDOSingleton::getInstance()->getConnexion());
+            $formHandler = new LoginFormHandler($loginForm, $manager, $this->httpRequest);
+            if ($formHandler->process()) {
+                return new HTTPResponse(
+                    $this->getPage(),
+                    [
+                        'messageLogin' => "Bonjour ". $this->httpRequest->getSession('user')->getPseudo() ." ! Vous êtes connecté.",
+                        'user' => $this->httpRequest->getSession('user')
+                    ]
+                );
+            }
+            return new HTTPResponse(
+                    $this->getPage(),
+                    ['messageLogin' => "Une erreur de pseudo ou de mot de passe.", 'loginForm' => $loginForm ]
+                );
+        }
+        
+        // Build empty login form
         $login = new Login();
         $FormBuilder = new LoginFormBuilder($login);
         $FormBuilder->build();
