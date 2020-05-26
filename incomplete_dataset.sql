baea153df1181f9aeba8c73cd65ba13a4abe8d04
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-26 18:24:48 +0200

    edit createdatabase.sql, incomplete_dataset.sql and  src/Entity/User.php, because the property enabled of User class is now 1 by default (and not 0), and can have only two values : 1 and 2

--- incomplete_dataset.sql
+++ incomplete_dataset.sql
@@ -10,10 +10,10 @@ INSERT INTO `permit` (`permit`) VALUES
 ('waiting');
 
 INSERT INTO `user` (`id`, `pseudo`, `password`, `email`, `activation_key`, `enabled`) VALUES
-(1, 'Admin', '$2y$10$HjZ9b2jDRjmFkq7ADDZO6O3CRDhfNWsjYSmRDU9rOGC34V3BNmIaW', ' ', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1),
-(2, 'Subscriber1', '$2y$10$.JW./lJMHdaz445iF/FtYuyEgK4hwiz03gkzb21UyhVYlZOY/A.o.', ' ', '91c52dbf5a162f3c86acc3573c30c41d20fb0116', 1),
-(3, 'Subscriber2', '$2y$10$OJeUngXjD7R2o4Mgv/fFpue4YGJRnTKabAXr7BshvVd0PpxWM8xBa', ' ', '65e64e4c90596adabf931564367bb93a005bedf0', 1),
-(4, 'Subscriber3', '$2y$10$CYEM2DTYo7AZs8/K37872eaeJVszZJZZRjb7VpJsUc/kio9rVbGjq', ' ', '1c47c74e7ce48e93909c2f22dd130257f872ad63', 0);
+(1, 'Admin', '$2y$10$HjZ9b2jDRjmFkq7ADDZO6O3CRDhfNWsjYSmRDU9rOGC34V3BNmIaW', ' ', 'd033e22ae348aeb5660fc2140aec35850c4da997', 2),
+(2, 'Subscriber1', '$2y$10$.JW./lJMHdaz445iF/FtYuyEgK4hwiz03gkzb21UyhVYlZOY/A.o.', ' ', '91c52dbf5a162f3c86acc3573c30c41d20fb0116', 2),
+(3, 'Subscriber2', '$2y$10$OJeUngXjD7R2o4Mgv/fFpue4YGJRnTKabAXr7BshvVd0PpxWM8xBa', ' ', '65e64e4c90596adabf931564367bb93a005bedf0', 2),
+(4, 'Subscriber3', '$2y$10$CYEM2DTYo7AZs8/K37872eaeJVszZJZZRjb7VpJsUc/kio9rVbGjq', ' ', '1c47c74e7ce48e93909c2f22dd130257f872ad63', 1);
 
 INSERT INTO `post` (`id`, `title`, `slug`, `content`, `resume`, `id_user`) VALUES
 (1, 'Titre du premier article', 'titre-du-premier-article', 'Contenu du premier article.\r\n\r\n\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"\r\n\r\n\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"', 'Un premier article comme support de travail à la construction du site', 1),
