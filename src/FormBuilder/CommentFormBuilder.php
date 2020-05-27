b55f76ffd0fbbf36f453963d0b25a96d4b098e79
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-27 14:12:00 +0200

    edit src/FormBuilder/CommentFormBuilder.php and templates/post.twig to add maxlength attribute and MaxLengthValidator to comment form

--- src/FormBuilder/CommentFormBuilder.php
+++ src/FormBuilder/CommentFormBuilder.php
@@ -4,8 +4,9 @@ namespace App\FormBuilder;
 
 use App\Application\Entity;
 use App\Application\Form\FormBuilder;
-use App\Application\Form\NotEmptyValidator;
 use App\Application\Form\TextareaField;
+use App\Application\Form\NotEmptyValidator;
+use App\Application\Form\MaxLengthValidator;
 //use App\Application\HTTPRequest;
 
 /**
@@ -22,10 +23,12 @@ final class CommentFormBuilder extends FormBuilder
                 'name' => 'content',
                 'idField' => 'content',
                 'placeholder' => '... ici pour écrire un commentaire ...',
+                'maxlength' => 1500,
                 'required' => 'required',
                 'rows' => 8,
                 'validators' => [
-                    new NotEmptyValidator('Merci d\'écrire un commentaire pour pouvoir l\'envoyer.')
+                    new NotEmptyValidator('Merci d\'écrire un commentaire pour pouvoir l\'envoyer.'),
+                    new MaxLengthValidator('Merci d\'écrire un commentaire de moins de 1500 caractères', 1500)
                 ],
             ])
         );
