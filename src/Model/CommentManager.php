c54b3874f23b54f25ecf3aafd38e79165276134e
CaroJo1986 <caroline.dirat@gmail.com>
2020-05-27 18:41:00 +0200

    edit src/Model/CommentManager.php and  src/Model/CommentManagerPDO.php to add save(Comment), add{Comment) and modify(Comment) methods

--- src/Model/CommentManager.php
+++ src/Model/CommentManager.php
@@ -4,6 +4,7 @@ namespace App\Model;
 
 use App\Application\Manager;
 use App\Entity\Comment;
+use Exception;
 
 /**
  * CommentManager.
@@ -29,4 +30,43 @@ abstract class CommentManager extends Manager
      * @return array[Comment]
      */
     abstract public function getValidComments(int $idPost): array;
+        
+    /**
+     * add
+     * 
+     * Method to add a comment in database
+     *
+     * @param  Comment $comment
+     * @return void
+     */
+    abstract public function add(Comment $comment): void;
+        
+    /**
+     * modify
+     * 
+     * Method to modify a comment in database
+     *
+     * @param  Comment $comment
+     * @return void
+     */
+    abstract public function modify(Comment $comment): void;
+    
+    /**
+     * save
+     * 
+     * Method wich save a comment in database :
+     * - add it if it's new
+     * - modify it if it isn't new
+     *
+     * @param  Comment $comment
+     * @return void
+     */
+    public function save(Comment $comment): void
+    {
+        if ($comment->isValid()) {
+            $comment->isNew() ? $this->add($comment) : $this->modify($comment);
+        } else {
+            throw new Exception("The comment must be valid to be saved.");
+        }
+    }
 }
