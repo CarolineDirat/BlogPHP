b23cd87b4209ab86e5f8de841db0f4cc23e0d15d
CaroJo1986 <caroline.dirat@gmail.com>
2020-06-02 08:52:56 +0200

    create   src/Application/Router.php : router class which stores routes and checks if a route matches one of those it stores

---
+++ src/Application/Router.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Application;
+
+use DOMDocument;
+use Exception;
+
+
+class Router 
+{    
+    /**
+     * routes
+     *
+     * @var Route[]
+     */
+    private array $routes = [];
+    
+    /**
+     * addRoute
+     * 
+     * add a route in the array routes property of the router
+     *
+     * @param  Route $route
+     * @return void
+     */
+    public function addRoute(Route $route): void
+    {
+        if (!in_array($route, $this->routes)) {
+            $this->routes[] = $route;
+        }
+
+    }
+        
+    /**
+     * loadRoutes
+     * 
+     * load routes in the router from configutation xml file
+     *
+     * @return void
+     */
+    public function loadRoutes(): void
+    {
+        $xml = new DOMDocument();
+        $xml->load(__DIR__.'/config/routes.xml');
+        $routes = $xml->getElementsByTagName('route');
+        foreach ($routes as $route) {
+            $this->addRoute(new Route(
+                $route->getAttribute('module'),
+                $route->getAttribute('action'),
+                $route->getAttribute('page'),
+            ));
+        }
+    }
+    
+    /**
+     * checkRoute
+     * 
+     * check if a route exists 
+     *
+     * @param  Route $route
+     * @return void
+     */
+    public function checkRoute(Route $route): void
+    {
+        $this->loadRoutes();
+        if (!in_array($route, $this->getRoutes())) {
+            throw new Exception("No Route corresponding to the Request");
+        }
+    }
+
+    /**
+     * Get routes
+     *
+     * @return  Route[]
+     */ 
+    public function getRoutes(): array
+    {
+        return $this->routes;
+    }
+}
+
+
+    
