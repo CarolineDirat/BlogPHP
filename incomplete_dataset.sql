c7925b7685767c4b6e9ddacf844f54137a73682c
CaroJo1986 <caroline.dirat@gmail.com>
2020-06-17 13:36:23 +0200

    improvement of the README.md file, and some points on installation guide

--- incomplete_dataset.sql
+++ incomplete_dataset.sql
@@ -25,11 +25,11 @@ INSERT INTO `permit` (`status`) VALUES
 ('valid'),
 ('waiting');
 
-INSERT INTO `user` (`id`, `pseudo`, `password`, `email`, `date_creation`, `activation_key`, `enabled`) VALUES
-(1, 'Admin', '$2y$10$rdf.N.ckoaS4e7dcKD/ndeAM8mVYpnSP0et7GaZ8TRo2RM496ka/C', '', '2020-04-24 11:49:10', 'd033e22ae348aeb5660fc2140aec35850c4da997', 2),
-(2, 'Subscriber1', '$2y$10$.JW./lJMHdaz445iF/FtYuyEgK4hwiz03gkzb21UyhVYlZOY/A.o.', '', '2020-04-24 12:49:10', '91c52dbf5a162f3c86acc3573c30c41d20fb0116', 2),
-(3, 'Subscriber2', '$2y$10$OJeUngXjD7R2o4Mgv/fFpue4YGJRnTKabAXr7BshvVd0PpxWM8xBa', '', '2020-04-24 13:49:10', '65e64e4c90596adabf931564367bb93a005bedf0', 2),
-(4, 'Subscriber3', '$2y$10$CYEM2DTYo7AZs8/K37872eaeJVszZJZZRjb7VpJsUc/kio9rVbGjq', '', '2020-04-24 15:49:10', '1c47c74e7ce48e93909c2f22dd130257f872ad63', 1);
+INSERT INTO `user` (`id`, `pseudo`, `password`, `date_creation`, `activation_key`, `enabled`) VALUES
+(1, 'Admin', '$2y$10$rdf.N.ckoaS4e7dcKD/ndeAM8mVYpnSP0et7GaZ8TRo2RM496ka/C', '2020-04-24 11:49:10', 'd033e22ae348aeb5660fc2140aec35850c4da997', 2),
+(2, 'Subscriber1', '$2y$10$.JW./lJMHdaz445iF/FtYuyEgK4hwiz03gkzb21UyhVYlZOY/A.o.', '2020-04-24 12:49:10', '91c52dbf5a162f3c86acc3573c30c41d20fb0116', 2),
+(3, 'Subscriber2', '$2y$10$OJeUngXjD7R2o4Mgv/fFpue4YGJRnTKabAXr7BshvVd0PpxWM8xBa', '2020-04-24 13:49:10', '65e64e4c90596adabf931564367bb93a005bedf0', 2),
+(4, 'Subscriber3', '$2y$10$CYEM2DTYo7AZs8/K37872eaeJVszZJZZRjb7VpJsUc/kio9rVbGjq', '2020-04-24 15:49:10', '1c47c74e7ce48e93909c2f22dd130257f872ad63', 1);
 
 INSERT INTO `post` (`id`, `title`, `slug`, `content`, `abstract`, `date_creation`, `date_update`, `id_user`) VALUES
 (1, 'Titre du premier article', 'titre-du-premier-article', 'Contenu du premier article.\r\n\r\n\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"\r\n\r\n\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"', 'Un premier article comme support de travail à la construction du site', '2020-04-24 11:49:10', '2020-04-24 11:49:10', 1),
