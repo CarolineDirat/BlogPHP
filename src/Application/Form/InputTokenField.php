2433bdde7e125c5c45f1b2a9aea4010d9f848f62
CaroJo1986 <caroline.dirat@gmail.com>
2020-06-18 14:43:58 +0200

    create App\Application\Form\InputTokenField which extends InputHiddenField

---
+++ src/Application/Form/InputTokenField.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Application\Form;
+
+/**
+ * InputTokenField.
+ * Represent an intput field with type="hidden", for the token CSRF
+ * 
+ */
+class InputTokenField extends InputHiddenField
+{
+    public function __construct(array $options = [])
+    {
+        parent::__construct($options);
+        $this->name = 'token';
+    }
+}
